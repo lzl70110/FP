76c607e669e363cf923d01229c350c8b44427e0c
Lyubomir Chekanski <50097367+lzl70110@users.noreply.github.com>
2026-08-24 21:00:49 +0300

    feat: add department CRUD actions

--- FP.Web/Controllers/DepartmentsController.cs
+++ FP.Web/Controllers/DepartmentsController.cs
@@ -1,21 +1,81 @@
-﻿using FP.Application.Contracts.Repositories;
+﻿using FP.Application.Contracts.Services;
 using FP.Domain.Entities.Departments;
 using Microsoft.AspNetCore.Mvc;
 
-namespace FP.web.Controllers;
+namespace FP.Web.Controllers;
+
 public class DepartmentsController : Controller
 {
-    private readonly IRepository<Department> repository;
+    private readonly IDepartmentService service;
 
-    public DepartmentsController(IRepository<Department> repository)
+    public DepartmentsController(IDepartmentService service)
     {
-        this.repository = repository;
+        this.service = service;
     }
 
     public async Task<IActionResult> Index()
     {
-        var departments = await repository.GetAllAsync();
+        var departments = await service.GetAllAsync();
 
         return View(departments);
+    }
+
+    public async Task<IActionResult> Details(int id)
+    {
+        var department = await service.GetByIdAsync(id);
+
+        if (department == null)
+        {
+            return NotFound();
+        }
+
+        return View(department);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(Department department)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(department);
+        }
+
+        await service.CreateAsync(department);
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Edit(int id)
+    {
+        var department = await service.GetByIdAsync(id);
+
+        if (department == null)
+        {
+            return NotFound();
+        }
+
+        return View(department);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(Department department)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(department);
+        }
+
+        await service.UpdateAsync(department);
+
+        return RedirectToAction(nameof(Index));
     }
 }
