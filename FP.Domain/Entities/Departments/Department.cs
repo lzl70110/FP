a5805da9dc16e45af2313bc5905aee928db06315
Lyubomir Chekanski <50097367+lzl70110@users.noreply.github.com>
2026-09-01 21:19:02 +0300

    ui: add department management dashboard

--- FP.Domain/Entities/Departments/Department.cs
+++ FP.Domain/Entities/Departments/Department.cs
@@ -22,6 +22,7 @@ public class Department : SoftDeletableEntity
         ErrorMessage = "Забележката не може да надвишава 1000 символа.")]
     public string? Notes { get; set; }
 
+    [Display(Name = "Активно")]
     public bool IsActive { get; set; } = true;
 
     public ICollection<Position> Positions { get; set; }
