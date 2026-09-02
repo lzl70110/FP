5ea93457c7f420e0a4208cadebeb975c45a55710
Lyubomir Chekanski <50097367+lzl70110@users.noreply.github.com>
2026-09-02 18:39:04 +0300

    Refactor Home branding and layout preparation

--- FP.Web/Models/Forms/NamedActiveEntityViewModel.cs
+++ FP.Web/Models/Forms/NamedActiveEntityViewModel.cs
@@ -1,10 +1,15 @@
-﻿namespace FP.Web.Models.Forms;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FP.Web.Models.Forms;
 
 public class NamedActiveEntityViewModel
 {
+    [Display(Name = "Наименование")]
     public string Name { get; set; } = null!;
 
+    [Display(Name = "Бележки")]
     public string? Notes { get; set; }
 
+    [Display(Name = "Активна")]
     public bool IsActive { get; set; }
 }
